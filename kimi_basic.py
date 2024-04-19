c132df3025d5fb4263bc24046ff6553d97e5dfa5
lemonhall <lemonhall2012@qq.com>
2024-04-19 09:38:00 +0800

    Create kimi_basic.py

---
+++ kimi_basic.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
+ 
+client = OpenAI(
+    api_key="sk-xxxxxxxxxxxxxxxxxxx",
+    base_url="https://api.moonshot.cn/v1",
+)
+ 
+history = [
+    {"role": "system", "content": "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，不可翻译成其他语言。"}
+]
+ 
+def chat(query, history):
+    history += [{
+        "role": "user", 
+        "content": query
+    }]
+    completion = client.chat.completions.create(
+        model="moonshot-v1-8k",
+        messages=history,
+        temperature=0.3,
+    )
+    result = completion.choices[0].message.content
+    history += [{
+        "role": "assistant",
+        "content": result
+    }]
+    return result
+
+while True:
+    user_input = input(Fore.GREEN+"你：")
+    print(Fore.RED+"Kimi:",chat(user_input, history))
